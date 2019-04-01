7d222f615e32933da1db406cecdd39224839e143
Papillori <43313101+Papillori@users.noreply.github.com>
2019-04-01 21:50:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -413,9 +413,8 @@ client.on("error", () => {
 });
 
 // Client token, required for the bot to work
-try {
     client.login(process.env.BOT_TOKEN);
-}
+
 catch (e) {
     console.log("ERROR: No account was linked to your bot. Please provide a valid authentication token. You can change it in the globalSettings.json file.")
 }
